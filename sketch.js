b86813e5f258fe3b6c737146690c7b35459de0b0
yadiel <yadield2889@tuckahoeschools.org>
2025-03-05 13:28:05 +0000

    idk

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup(){
-    createCanvas(400, 400);
+    createCanvas(400, 400, WEBGL);
 }
 
 function draw(){
     background(255, 0 , 200);
+
+    rotateY(frameCount * 0.01);
+
+  // Draw the triangle.
+  triangle(-20, 25, 8, -30, 36, 25);
+
 }
+
